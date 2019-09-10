99e409c9cbabede38fd903337e683270ee9f13a3
Andriy <valia.overchuk@gmail.com>
2019-09-11 00:31:16 +0300

    second line

---
+++ ISMSecondConsoleProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISMSecondConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Second Project");
+        }
+    }
+}
